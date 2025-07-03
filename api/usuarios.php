14b0a4b2c8b0b206782a0c4dd982a2c7cb0b4360
Soledad Albornoz <albornoz.soledad@gmail.com>
2025-07-03 09:12:18 -0300

    first commit

---
+++ api/usuarios.php
@@ -0,0 +1,10 @@
+<?php
+$conexion = new mysqli("localhost", "phpmyadmin", "sole0504", "fotomascotas");
+$consulta = $conexion->query("SELECT nombre, usuario FROM usuarios");
+$usuarios = [];
+while ($fila = $consulta->fetch_assoc()) {
+    $usuarios[] = $fila;
+}
+header('Content-Type: application/json');
+echo json_encode($usuarios);
+?>
